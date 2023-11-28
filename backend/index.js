7126d56050e8527150319a8b2ef142d783658879
ericjen01 <ericjen01>
2023-11-28 14:53:33 -0800

    3.12: Command-line database

--- backend/index.js
+++ backend/index.js
@@ -12,29 +12,6 @@ app.use(express.json());
 app.use(morgan("method-:method, status-:status, url-:url, body-:body"));
 app.use(express.static("dist"));
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 const generateId = () => {
   const maxId = persons.length > 0 ? Math.max(...persons.map((p) => p.id)) : 0;
   return maxId + 1;
